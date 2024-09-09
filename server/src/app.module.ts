2c7daf21027c930cbb355ee0c5a394125980b47c
tkddnr1022@student.jbnu.ac.kr <tkddnr1022@student.jbnu.ac.kr>
2024-09-09 09:35:17 +0900

    install mongoose, swagger, event-emitter

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { SwaggerModule } from '@nestjs/swagger';
+import { EventEmitterModule } from '@nestjs/event-emitter';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({
+    isGlobal: true
+  }),
+  MongooseModule.forRootAsync({
+    imports: [ConfigModule],
+    useFactory: async (configService: ConfigService) => ({
+      uri: `mongodb+srv://${encodeURIComponent(configService.get<string>('MONGO_USERNAME'))}:${encodeURIComponent(configService.get<string>('MONGO_PASSWORD'))}@${configService.get<string>('MONGO_URI')}`,
+    }),
+    inject: [ConfigService],
+  }),
+    SwaggerModule,
+  EventEmitterModule.forRoot(),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
