577bd25d81e2894e5de67b1bbaa9443ad8adf460
tkddnr1022 <tkddnr1022@naver.com>
2024-09-24 01:15:47 +0900

    Feat: add userId validation for command

--- server/src/auth/guard/roles.guard.ts
+++ server/src/auth/guard/roles.guard.ts
@@ -9,12 +9,21 @@ export class RolesGuard implements CanActivate {
 
     canActivate(context: ExecutionContext): boolean {
         const requiredRoles = this.reflector.get<Role[]>(ROLES_KEY, context.getHandler());
-        if (!requiredRoles) {
-            return true;
-        }
-
         const request = context.switchToHttp().getRequest();
         const user = request.user;
+
+        if(user.roles.includes(Role.Admin)){
+            return true;    // 관리자일 경우 통과
+        }
+        if(request.params.userId && request.params.userId != user.userId){
+            return false;   // 관리자가 아닌 경우 타인의 유저 정보 접근 방지
+        }
+        // Todo: 수업 정보 Command 요청 시 userId 검사
+        /*
+        if(request.params.courseId && course.userId != user.userId){
+            return false;   // 관리자가 아닌 경우 타인의 수업 정보 접근 방지
+        }
+        */
 
         return requiredRoles.some((role) => user.roles.includes(role));
     }
