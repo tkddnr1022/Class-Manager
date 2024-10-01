f3f166a6d4329052d37e8c040fb3e1b8f0356e3c
tkddnr1022 <tkddnr1022@naver.com>
2024-10-02 03:49:20 +0900

    Feat: add class validation to dto

--- server/src/course/dtos/create-course.dto.ts
+++ server/src/course/dtos/create-course.dto.ts
@@ -1,8 +1,19 @@
+import { IsDate, IsNotEmpty, IsNotEmptyObject, IsString } from "class-validator";
 import Coordinate from "../interfaces/coordinate.interface";
 
 export class CreateCourseDto {
+    @IsString()
+    @IsNotEmpty()
     readonly title: string;
+
+    @IsDate()
+    @IsNotEmpty()
     readonly startAt: Date;
+
+    @IsDate()
+    @IsNotEmpty()
     readonly endAt: Date;
+
+    @IsNotEmptyObject()
     readonly location: Coordinate;
 }
--- server/src/course/dtos/update-course.dto.ts
+++ server/src/course/dtos/update-course.dto.ts
@@ -1,8 +1,16 @@
+import { IsDate, IsObject, IsString } from "class-validator";
 import Coordinate from "../interfaces/coordinate.interface";
 
 export class UpdateCourseDto {
+    @IsString()
     public readonly title?: string;
+
+    @IsDate()
     public readonly startAt?: Date;
+
+    @IsDate()
     public readonly endAt?: Date;
+
+    @IsObject()
     public readonly location?: Coordinate;
 }
