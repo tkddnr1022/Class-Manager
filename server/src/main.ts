56e343d89eaa3c2cc416522dabd14d810a78f729
tkddnr1022 <tkddnr1022@naver.com>
2024-10-08 02:42:04 +0900

    Feat: setup swagger module

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,14 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  // Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Class-Manager')
+    .setDescription('위치 기반 QR 출석 서비스')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+
+  // class-validator
   app.useGlobalPipes(new ValidationPipe({
     transform: true,
     whitelist: true,
     forbidNonWhitelisted: true,
   }));
+
   await app.listen(3000);
 }
 bootstrap();
