34edbe3d891c1747fa4fff0a2ee97e8426276382
tkddnr1022 <tkddnr1022@naver.com>
2024-10-15 06:05:54 +0900

    Feat: add home page

--- client/app/(tabs)/home.tsx
+++ client/app/(tabs)/home.tsx
@@ -4,18 +4,38 @@ import { Button, Card, Text } from "react-native-paper";
 
 export default function Home() {
 
-    const cameraHandler = () => {
+    const goToCamera = () => {
         router.navigate('/camera');
     }
 
     return (
         <View style={styles.container}>
-            <Button mode="contained" onPress={cameraHandler}>카메라</Button>
-            <Card>
+            <Card style={styles.card}>
+                <Card.Title title="변경사항" titleVariant="titleMedium"/>
                 <Card.Content>
-                    <Text>test</Text>
+                    <Text>- 최초 버전</Text>
+                    <Text>- 출석 및 수업 관리</Text>
                 </Card.Content>
             </Card>
+            <Card style={styles.card}>
+                <Card.Title title="구현예정" titleVariant="titleMedium"/>
+                <Card.Content>
+                    <Text>- 소셜 로그인</Text>
+                    <Text>- 헤더 뒤로가기 버튼</Text>
+                    <Text>- 웹 최적화</Text>
+                    <Text>- 다양한 관리 기능</Text>
+                    <Text>- UI 개선</Text>
+                </Card.Content>
+            </Card>
+            <Card style={styles.card}>
+                <Card.Title title="이슈" titleVariant="titleMedium"/>
+                <Card.Content>
+                    <Text>- 권한 거부 시 수동 설정 필요</Text>
+                    <Text>- 네트워크 에러 예외처리 부족</Text>
+                    <Text>- 리스트 가독성 개선 필요</Text>
+                </Card.Content>
+            </Card>
+            <Button mode="contained" onPress={goToCamera} icon="camera" labelStyle={{lineHeight: 16}} style={styles.button}>카메라 열기</Button>
         </View>
     );
 }
@@ -23,7 +43,14 @@ export default function Home() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
+        padding: 32,
     },
+    card: {
+        width: "100%",
+        marginBottom: 24,
+    },
+    button: {
+        paddingVertical: 4,
+        borderRadius: 12,
+    }
 });
