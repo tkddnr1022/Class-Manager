46358183f55ab9adc0a397a15789a8a560391f8f
tkddnr1022 <tkddnr1022@naver.com>
2024-10-21 02:59:25 +0900

    Feat: add kakao login

--- client/app/login.tsx
+++ client/app/login.tsx
@@ -1,19 +1,90 @@
 import { signIn, getProfile } from '@/scripts/api/auth';
 import { setStorageProfile, setStorageToken } from '@/scripts/utils/storage';
 import { router } from 'expo-router';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, StyleSheet } from 'react-native';
 import { TextInput, Button, Text, Card } from 'react-native-paper';
 import Toast from 'react-native-toast-message';
+import * as Linking from 'expo-linking';
+
+const KAKAO_CLIENT_ID = process.env.EXPO_PUBLIC_KAKAO_CLIENT_ID;
+const API_URL = process.env.EXPO_PUBLIC_API_URL;
 
 export default function Login() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [loading, setLoading] = useState(false);
 
+    useEffect(() => {
+        Linking.getInitialURL().then((url) => {
+            if (url) {
+                handleRedirect(url);
+            }
+        });
+        Linking.addEventListener('url', (event) => handleRedirect(event.url));
+        return () => {
+            // Todo: RemoveEventListener
+        };
+    }, []);
+
     const handleLogin = async () => {
         setLoading(true);
         const token = await signIn(email, password);
+        if (token) {
+            try {
+                await setStorageToken(token);
+                const profile = await getProfile();
+                if (profile) {
+                    await setStorageProfile(profile);
+                }
+                Toast.show({
+                    type: 'success',
+                    text1: '로그인 성공',
+                });
+                router.replace('/(tabs)/home');
+            } catch (error) {
+                console.error(error);
+                setLoading(false);
+            }
+        } else {
+            Toast.show({
+                type: 'error',
+                text1: '로그인 실패',
+                text2: '이메일 혹은 비밀번호가 올바르지 않습니다.'
+            });
+            setLoading(false);
+        }
+    };
+
+    const handleSignUp = () => {
+        router.push('/signup');
+    }
+
+    const handleKakao = async () => {
+        try {
+            router.push({
+                pathname: 'https://kauth.kakao.com/oauth/authorize',
+                params: {
+                    client_id: KAKAO_CLIENT_ID,
+                    redirect_uri: `${API_URL}/auth/kakao`,
+                    response_type: 'code',
+                },
+            });
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    // Todo: 구글 로그인과 분리
+    // Todo: 이름, 학번 입력 페이지 이동 및 입력 여부 검사(home)
+    const handleRedirect = async (url: string) => {
+        setLoading(true);
+        const { queryParams } = Linking.parse(url);
+        console.log(queryParams);
+        const token = {
+            access_token: queryParams?.access_token as string,
+            refresh_token: queryParams?.refresh_token as string,
+        };
         if (token) {
             try {
                 await setStorageToken(token);
@@ -30,19 +101,15 @@ export default function Login() {
                 console.error(error);
                 setLoading(false);
             }
-        } else {
+        }
+        else {
             Toast.show({
                 type: 'error',
                 text1: '로그인 실패',
-                text2: '이메일 혹은 비밀번호가 올바르지 않습니다.'
-            })
+            });
             setLoading(false);
         }
     };
-
-    const handleSignUp = () => {
-        router.push('/signup');
-    }
 
     return (
         <View style={styles.container}>
@@ -85,6 +152,16 @@ export default function Login() {
                         contentStyle={styles.buttonContent}
                     >
                         회원가입
+                    </Button>
+                    <Button
+                        mode="contained"
+                        disabled={loading}
+                        loading={loading}
+                        onPress={handleKakao}
+                        style={styles.button}
+                        contentStyle={styles.buttonContent}
+                    >
+                        kakao_test
                     </Button>
                 </Card.Content>
             </Card>
--- client/interfaces/user.ts
+++ client/interfaces/user.ts
@@ -1,7 +1,7 @@
 export default interface User{
     _id: string;
-    username: string;
     email: string;
-    studentId: string;
     roles: string[];
+    studentId?: string;
+    username?: string;
 }
