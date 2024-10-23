23162cbe4173cf97133e5fb089d0c2da37b0372d
tkddnr1022 <tkddnr1022@naver.com>
2024-10-24 03:29:54 +0900

    Design: use text component from react-native-paper

--- client/app/(tabs)/course/qrcode/[id].tsx
+++ client/app/(tabs)/course/qrcode/[id].tsx
@@ -1,7 +1,7 @@
 import { useLocalSearchParams } from 'expo-router';
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { Card } from 'react-native-paper';
+import { View, StyleSheet } from 'react-native';
+import { Card, Text } from 'react-native-paper';
 import QRCode from 'react-native-qrcode-svg';
 
 export default function Qrcode() {
